cfbed3345cb52a099dde96f2cca59ecca2d2731d
rafaelfachinelli <rafael.fachinelli@hotmail.com>
2022-05-29 20:52:29 -0300

    shuffling

--- scripts.js
+++ scripts.js
@@ -50,4 +50,11 @@ function resetBoard() {
     [firstCard, secondCard] = [null, null]
 }
 
+(function shuffle() {
+    cards.forEach((card) => {
+        const randomPos = Math.floor(Math.random() * 12)
+        card.style.order = randomPos
+    })
+})()
+
 cards.forEach((card) => card.addEventListener("click", flipCard) )
