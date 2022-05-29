02b67f016c7a484a78b053f1d8488e23a28ccfd0
rafaelfachinelli <rafael.fachinelli@hotmail.com>
2022-05-29 20:27:30 -0300

    store cards

--- scripts.js
+++ scripts.js
@@ -1,7 +1,18 @@
 const cards = document.querySelectorAll(".memory-card")
 
+let hasFlippedCard = false
+let firstCard, secondCard
+
 function flipCard() {
     this.classList.toggle("flip")
+
+    if (!hasFlippedCard) {
+        hasFlippedCard = true
+        firstCard = this
+    } else {
+        hasFlippedCard = false
+        secondCard = this
+    }
 }
 
 cards.forEach((card) => card.addEventListener("click", flipCard) )
